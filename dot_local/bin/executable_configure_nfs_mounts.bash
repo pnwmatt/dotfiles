bec52501c26852c382f29df6f0abd30743042db9
auricom <27022259+auricom@users.noreply.github.com>
2024-05-12 22:42:34 +0200

    feat: rename nfs shares

--- dot_local/bin/executable_configure_nfs_mounts.bash
+++ dot_local/bin/executable_configure_nfs_mounts.bash
@@ -7,14 +7,14 @@ if [[ "$1" != "--run" ]]; then
     exit 0
 fi
 
-TRUENAS_IP="192.168.9.10"
-SHARE_PREFIX="/mnt/storage"
-MOUNTPOINT_PREFIX="/var/mnt/truenas"
+NFS_SERVER_IP="192.168.9.10"
+SHARE_PREFIX="/var/mnt/vol1"
+MOUNTPOINT_PREFIX="/var/mnt/storage.feisar.ovh"
 OPTS="nfsvers=4,soft,timeo=150,retrans=10,_netdev,bg"
 
 should_backup=true
 
-echo -e "\033[0;32m>>>>> Configure NFS Mounts for TrueNAS <<<<<\033[0m"
+echo -e "\033[0;32m>>>>> Configure NFS Mounts <<<<<\033[0m"
 
 # Function to add an entry to /etc/fstab if it does not already exists based on share name
 add_fstab_entry() {
@@ -44,16 +44,16 @@ add_fstab_entry() {
     fi
 }
 
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/apps" "${MOUNTPOINT_PREFIX}/apps" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/photo" "${MOUNTPOINT_PREFIX}/photo" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/video" "${MOUNTPOINT_PREFIX}/video" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/shared-documents" "${MOUNTPOINT_PREFIX}/shared-documents" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/backups" "${MOUNTPOINT_PREFIX}/backups" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/music_transcoded" "${MOUNTPOINT_PREFIX}/music_transcoded" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/downloads" "${MOUNTPOINT_PREFIX}/downloads" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/home/helene" "${MOUNTPOINT_PREFIX}/home-helene" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/home/claude" "${MOUNTPOINT_PREFIX}/home-claude" "${OPTS}" 0 0
-add_fstab_entry "${TRUENAS_IP}:${SHARE_PREFIX}/music" "${MOUNTPOINT_PREFIX}/music" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/apps" "${MOUNTPOINT_PREFIX}/apps" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/photo" "${MOUNTPOINT_PREFIX}/photo" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/video" "${MOUNTPOINT_PREFIX}/video" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/shared-documents" "${MOUNTPOINT_PREFIX}/shared-documents" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/backups" "${MOUNTPOINT_PREFIX}/backups" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/music_transcoded" "${MOUNTPOINT_PREFIX}/music_transcoded" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/downloads" "${MOUNTPOINT_PREFIX}/downloads" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/home/helene" "${MOUNTPOINT_PREFIX}/home-helene" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/home/claude" "${MOUNTPOINT_PREFIX}/home-claude" "${OPTS}" 0 0
+add_fstab_entry "${NFS_SERVER_IP}:${SHARE_PREFIX}/music" "${MOUNTPOINT_PREFIX}/music" "${OPTS}" 0 0
 
 if ! ${should_backup}; then
     systemctl daemon-reload
