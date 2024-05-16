c8b676f673141353a4e12fff48c0dc0667fb166b
auricom <27022259+auricom@users.noreply.github.com>
2024-05-16 08:45:30 +0200

    feat: tweaks for ucore

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo -e "\033[0;32m>>>>> Start bluefin-cli <<<<<\033[0m"
 
 mkdir -p ~/.config/containers/systemd
 
-wget --quiet --output-document ~/.config/containers/systemd/bluefin-cli.container \
+curl --silent --output ~/.config/containers/systemd/bluefin-cli.container \
     https://raw.githubusercontent.com/auricom/dotfiles/main/private_dot_config/private_containers/systemd/bluefin-cli.container.tmpl
 
 systemctl --user daemon-reload
