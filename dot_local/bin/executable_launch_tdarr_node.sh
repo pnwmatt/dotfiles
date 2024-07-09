d07ca39fbc29e1cff88dbca743ca48d113766415
auricom <27022259+auricom@users.noreply.github.com>
2024-07-10 00:35:41 +0200

    feat: remove OpenLens package

---
+++ dot_local/bin/executable_launch_tdarr_node.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Check if the tdarr-node container is running
+if podman ps --format '{{.Names}}' | grep -q '^tdarr-node$'; then
+    echo "tdarr-node container is running. No action taken."
+else
+    echo "tdarr-node container is not running. Starting tdarr-node.service..."
+    systemctl --user start tdarr-node.service
+fi
