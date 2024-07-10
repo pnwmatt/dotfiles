bcedb03c3b1b4b2b485e29ff95e21a6a5ad597b5
auricom <27022259+auricom@users.noreply.github.com>
2024-07-10 16:43:57 +0200

    feat: update tdarr script

--- dot_local/bin/executable_launch_tdarr_node.sh
+++ dot_local/bin/executable_launch_tdarr_node.sh
@@ -2,7 +2,8 @@
 
 # Check if the tdarr-node container is running
 if podman ps --format '{{.Names}}' | grep -q '^tdarr-node$'; then
-    echo "tdarr-node container is running. No action taken."
+    echo "tdarr-node container is running. Stoping tdarr-node.service..."
+    systemctl --user stop tdarr-node.service
 else
     echo "tdarr-node container is not running. Starting tdarr-node.service..."
     systemctl --user start tdarr-node.service
