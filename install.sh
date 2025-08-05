873cb0bcc354ccff2b0342c23f32dccbb9f9875e
pnwmatt <180812017+pnwmatt@users.noreply.github.com>
2025-08-05 07:09:44 -0700

    tz

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
+sudo timedatectl set-timezone America/Los_Angeles
 
 NONINTERACTIVE=true /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
