bb9a02b9c0031ba286e2ab0215fdc5d2d97ce6ed
pnwmatt <180812017+pnwmatt@users.noreply.github.com>
2025-08-05 06:47:53 -0700

    add ruby

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# key file for rvm
+gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+
 NONINTERACTIVE=true /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
