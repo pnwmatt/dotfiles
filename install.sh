456ccc576aa75fbb80feda22dd7db079e66e1850
pnwmatt <180812017+pnwmatt@users.noreply.github.com>
2025-08-05 00:13:24 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+brew install chezmoi
+chezmoi init --apply
