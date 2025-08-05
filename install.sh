ffe47a78d0c311c8284e484ad01e685563edf364
pnwmatt <180812017+pnwmatt@users.noreply.github.com>
2025-08-05 07:13:49 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 # key file for rvm
-gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
 sudo timedatectl set-timezone America/Los_Angeles
 
-NONINTERACTIVE=true /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 brew install chezmoi
 chezmoi init --apply
